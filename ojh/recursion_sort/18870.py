704c15279da0bb1ee86dc9836ed7b321a84312de
Ohjunghh <cmt5ttt@gmail.com>
2025-04-07 00:44:51 +0900

    [ojh] boj: complete 18870.py

--- ojh/recursion_sort/18870.py
+++ ojh/recursion_sort/18870.py
@@ -4,7 +4,7 @@ input=sys.stdin.readline
 n = int(input())
 nums = list(map(int,input().split()))
 sort_nums=sorted(list(set(nums)))
-result = []
-for i in range(n):
-    result.append(sort_nums.index(nums[i]))
-print(*result)
+result = {num:i for i,num in enumerate(sort_nums)}
+
+for num in nums:
+    print(result[num],end=' ')
